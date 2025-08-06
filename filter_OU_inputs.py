f8dce5f38c9a3897149267da04dbeeebd5830788
Daniele Linaro <danielelinaro@gmail.com>
2025-08-06 11:48:31 +0200

    Simple PF models now working

--- filter_OU_inputs.py
+++ filter_OU_inputs.py
@@ -21,7 +21,7 @@ iter_fun = lambda it: tqdm(it, ascii=True, ncols=70)
 __all__ = ['run_vf', 'run_welch']
 
 
-def run_vf(X, F, n_poles, n_iter=4, weights=None, poles_guess=None, do_plot=False):
+def run_vf(X, F, n_poles, n_iter=3, weights=None, poles_guess=None, do_plot=False):
     """
     Runs the vector fitting algorithm for a given number of poles
     """
@@ -199,11 +199,11 @@ if __name__ == '__main__':
     N_vars = len(var_names)
 
     # these are the loads that should be stochastic in this simulation
-    load_names = config['load_names']
+    load_names = config['input_loads']
     N_loads = len(load_names)
     # these are the loads for which individual TFs were computed by compute_spectra.py:
     # they are NOT necessarily all the loads that are present in the power network
-    all_load_names = data['load_names'].tolist()
+    all_load_names = data['input_loads'].tolist()
     loads_idx = np.array([all_load_names.index(load_name) for load_name in load_names])
     # mean has to be zero because we are simulating small signal fluctuations around the mean
     mu = np.zeros(N_loads)
@@ -261,7 +261,7 @@ if __name__ == '__main__':
         y_all = []
         for S,u in zip(systems[i],U):
             _,y,_ = lsim(S,u,time)
-            assert y.imag.max() < 1e-10
+            assert y.imag.max() < 1e-6
             y_all.append(y.real)
         Y[i,:] = np.sum(y_all, axis=0)
 
